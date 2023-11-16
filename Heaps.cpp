046d5314511b127148cdcb9cb293279a49b192fa
Nicolas Said <Nicolasleesaid@gmail.com>
2023-11-16 02:09:30 -0500

    Added Heaps.cpp and Trees.cpp and updated README with instructions
    Added files to allow students to use for review for CS310 quiz 3 along with instructions to guide practice

---
+++ Heaps.cpp
@@ -0,0 +1,61 @@
+
+
+/**
+ * QUESTIONS:
+ * What type of tree is required to implement a heap?
+ *    Why does it matter? (For what operations to function
+ *    do we need that type of tree)
+ * 
+ * How are BSTs and Heaps similar? How are they different?
+ * 
+ * What is the height of a Heap with N nodes in it?
+ * 
+ * Describe the process of inserting a new value into a max-heap
+ * 
+ * What is the runtime complexity of insert()? Why?
+ *    
+ *    BONUS: Can you think of a case when insert() might be
+ *           especially slow? Think Dynamic Arrays... 
+ * 
+ * Describe the process of removing the top value from a max-heap
+ * 
+ * What is the runtime complexity of remove()? Why?
+ * 
+ * Describe the process of building a max-heap from an array of
+ *    unordered values
+ * 
+ * What is the runtime complexity of heapify()? Why?
+ * 
+ */
+
+/**
+ * QUESTIONS:
+ * 
+ * 10. Describe the heapsort algorithm
+ * 
+ * 11. What is the runtime complexity of heapsort()?
+ * 
+ * 12. What is the runtime complexity of calling build-max-heap()
+ *     and then calling heapsort() on the max-heap?
+ */
+
+/**
+ * QUESTIONS:
+ * 
+ * 13. Given the following heap, remove once and then insert the value
+ *     8 once.
+ * 
+                   ┌─┐
+                   │9│
+              ┌────┴─┴────┐
+              │           │
+             ┌▼┐         ┌▼┐
+             │7│         │5│
+         ┌───┴─┴──┐    ┌─┴─┘
+         │        │    │
+        ┌▼┐      ┌▼┐  ┌▼┐
+        │1│      │4│  │2│
+        └─┘      └─┘  └─┘
+
+ *
+ */
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ETWeek10
+
+<p>First, start with Trees.cpp for a review of binary search trees.</p>
+
+<p>Then, work on Heaps.cpp after we discuss the properties of a heap</p>
